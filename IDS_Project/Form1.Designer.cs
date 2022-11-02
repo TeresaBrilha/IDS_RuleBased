21a39ec5e484a70dcb30a989f0f3ccb037a12a3b
Teresa <2191207@my.ipleiria.pt>
2022-11-02 17:54:32 +0000

    Sniffer Filters TCP/UDP

--- IDS_Project/Form1.Designer.cs
+++ IDS_Project/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace IDS_Project
 {
-    partial class Form1
+    partial class IDS
     {
         /// <summary>
         /// Required designer variable.
@@ -35,14 +35,20 @@
             this.label2 = new System.Windows.Forms.Label();
             this.tbPackets = new System.Windows.Forms.RichTextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.btnStopSniffing = new System.Windows.Forms.Button();
+            this.btnTcp = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnRemoveFilters = new System.Windows.Forms.Button();
+            this.btnUDP = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // StartSnifferBtn
             // 
-            this.StartSnifferBtn.BackColor = System.Drawing.Color.LightCoral;
-            this.StartSnifferBtn.Location = new System.Drawing.Point(318, 12);
+            this.StartSnifferBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.StartSnifferBtn.Location = new System.Drawing.Point(15, 9);
+            this.StartSnifferBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.StartSnifferBtn.Name = "StartSnifferBtn";
-            this.StartSnifferBtn.Size = new System.Drawing.Size(132, 61);
+            this.StartSnifferBtn.Size = new System.Drawing.Size(156, 53);
             this.StartSnifferBtn.TabIndex = 0;
             this.StartSnifferBtn.Text = "Start Sniffing";
             this.StartSnifferBtn.UseVisualStyleBackColor = false;
@@ -50,59 +56,122 @@
             // 
             // TbDevices
             // 
-            this.TbDevices.Location = new System.Drawing.Point(12, 130);
+            this.TbDevices.Location = new System.Drawing.Point(16, 160);
+            this.TbDevices.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TbDevices.Name = "TbDevices";
-            this.TbDevices.Size = new System.Drawing.Size(776, 96);
+            this.TbDevices.Size = new System.Drawing.Size(1033, 134);
             this.TbDevices.TabIndex = 1;
             this.TbDevices.Text = "";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 114);
+            this.label1.Location = new System.Drawing.Point(16, 140);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.Size = new System.Drawing.Size(57, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "Devices";
             // 
             // TbVersion
             // 
-            this.TbVersion.Location = new System.Drawing.Point(122, 88);
+            this.TbVersion.Location = new System.Drawing.Point(147, 109);
+            this.TbVersion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TbVersion.Name = "TbVersion";
-            this.TbVersion.Size = new System.Drawing.Size(100, 20);
+            this.TbVersion.Size = new System.Drawing.Size(132, 22);
             this.TbVersion.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 91);
+            this.label2.Location = new System.Drawing.Point(12, 112);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.Size = new System.Drawing.Size(127, 16);
             this.label2.TabIndex = 4;
             this.label2.Text = "SharpPcap Version:";
             // 
             // tbPackets
             // 
-            this.tbPackets.Location = new System.Drawing.Point(12, 259);
+            this.tbPackets.Location = new System.Drawing.Point(13, 332);
+            this.tbPackets.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbPackets.Name = "tbPackets";
-            this.tbPackets.Size = new System.Drawing.Size(776, 96);
+            this.tbPackets.Size = new System.Drawing.Size(1033, 209);
             this.tbPackets.TabIndex = 5;
             this.tbPackets.Text = "";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 243);
+            this.label3.Location = new System.Drawing.Point(12, 312);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.Size = new System.Drawing.Size(56, 16);
             this.label3.TabIndex = 6;
             this.label3.Text = "Packets";
             // 
-            // Form1
+            // btnStopSniffing
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.btnStopSniffing.BackColor = System.Drawing.Color.LightCoral;
+            this.btnStopSniffing.Location = new System.Drawing.Point(179, 9);
+            this.btnStopSniffing.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStopSniffing.Name = "btnStopSniffing";
+            this.btnStopSniffing.Size = new System.Drawing.Size(169, 53);
+            this.btnStopSniffing.TabIndex = 7;
+            this.btnStopSniffing.Text = "Stop Sniffing";
+            this.btnStopSniffing.UseVisualStyleBackColor = false;
+            this.btnStopSniffing.Click += new System.EventHandler(this.btnStopSniffing_Click);
+            // 
+            // btnTcp
+            // 
+            this.btnTcp.Location = new System.Drawing.Point(743, 12);
+            this.btnTcp.Name = "btnTcp";
+            this.btnTcp.Size = new System.Drawing.Size(95, 39);
+            this.btnTcp.TabIndex = 8;
+            this.btnTcp.Text = "TCP";
+            this.btnTcp.UseVisualStyleBackColor = true;
+            this.btnTcp.Click += new System.EventHandler(this.btnTcp_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(562, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Filtros";
+            // 
+            // btnRemoveFilters
+            // 
+            this.btnRemoveFilters.Location = new System.Drawing.Point(611, 12);
+            this.btnRemoveFilters.Name = "btnRemoveFilters";
+            this.btnRemoveFilters.Size = new System.Drawing.Size(126, 39);
+            this.btnRemoveFilters.TabIndex = 11;
+            this.btnRemoveFilters.Text = "Remove Filters";
+            this.btnRemoveFilters.UseVisualStyleBackColor = true;
+            this.btnRemoveFilters.Click += new System.EventHandler(this.btnRemoveFilters_Click);
+            // 
+            // btnUDP
+            // 
+            this.btnUDP.Location = new System.Drawing.Point(844, 12);
+            this.btnUDP.Name = "btnUDP";
+            this.btnUDP.Size = new System.Drawing.Size(95, 39);
+            this.btnUDP.TabIndex = 12;
+            this.btnUDP.Text = "UDP";
+            this.btnUDP.UseVisualStyleBackColor = true;
+            this.btnUDP.Click += new System.EventHandler(this.btnUDP_Click);
+            // 
+            // IDS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.MediumPurple;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btnUDP);
+            this.Controls.Add(this.btnRemoveFilters);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnTcp);
+            this.Controls.Add(this.btnStopSniffing);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.tbPackets);
             this.Controls.Add(this.label2);
@@ -110,8 +179,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.TbDevices);
             this.Controls.Add(this.StartSnifferBtn);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "IDS";
+            this.Text = "IDS";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -126,6 +196,11 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.RichTextBox tbPackets;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStopSniffing;
+        private System.Windows.Forms.Button btnTcp;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnRemoveFilters;
+        private System.Windows.Forms.Button btnUDP;
     }
 }
 
--- IDS_Project/Form1.cs
+++ IDS_Project/Form1.cs
@@ -1,4 +1,6 @@
-﻿using SharpPcap;
+﻿using PacketDotNet;
+using SharpPcap;
+using SharpPcap.LibPcap;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -13,21 +15,32 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 
 namespace IDS_Project
 {
-    public partial class Form1 : Form
+    public partial class IDS : Form
     {
+        public bool isSniffing { get; set; }
+        public string filters { get; set; }
+        delegate void SetTextCallback(RawCapture text);
+        public ICaptureDevice deviceCaptured { get; set; }
 
-        public Form1()
+        public IDS()
         {
             InitializeComponent();
         }
 
         private void StartSnifferBtn_Click(object sender, EventArgs e)
         {
+            //Start Sniffing
+            isSniffing = true;
+            filters = string.Empty;
+            //Display ShapPcap Version
             TbVersion.Text = SharpPcap.Pcap.Version;
 
+            //Display Devices in the network
             var devices = CaptureDeviceList.Instance;
 
             if (devices.Count == 0)
@@ -39,13 +52,124 @@ namespace IDS_Project
                 TbDevices.AppendText("Devices found in the network:\n");
                 foreach (ICaptureDevice device in devices)
                 {
-                    TbDevices.AppendText($"{device.ToString()}");
+                    TbDevices.AppendText($"Name:{device.Name}\nDescription:{device.Description}\n" +
+                        $"MAC:{device.MacAddress}\n");
                     TbDevices.AppendText("---------------------------------\n");
 
                 }
             }
 
+            //Display Packets
+            
+                foreach (ICaptureDevice device in devices)
+                {
+                    //Open device for capture
+                    int readTimeoutMilliseconds = 1000;
+                    device.Open(DeviceModes.Promiscuous, readTimeoutMilliseconds);
+                    tbPackets.AppendText($"-- Listening on {device.Description}\n");
+                    device.OnPacketArrival += Device_OnPacketArrival;
+                    device.StartCapture();
+                    
+                }   
+            
+
         }
 
+        private void changePacketText(RawCapture packetResult)
+        {
+            if (this.tbPackets.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(changePacketText);
+                this.Invoke(d, new object[] { packetResult });
+            }
+            else
+            {
+                if(deviceCaptured.Filter == string.Empty)
+                {
+                    DateTime time = packetResult.Timeval.Date;
+                    int lenPacket = packetResult.Data.Length;
+                    tbPackets.AppendText($"Time:{time.Hour}:{time.Minute}:{time.Second} | Package Len = {lenPacket}\n");
+                }
+                if(deviceCaptured.Filter.Equals("ip and tcp"))
+                {
+                    var tcp = (TcpPacket)packetResult.GetPacket().Extract<TcpPacket>();
+                    if(tcp != null)
+                    {
+                        DateTime time = packetResult.Timeval.Date;
+                        int lenPacket = packetResult.Data.Length;
+                        string srcPort = tcp.SourcePort.ToString();
+                        string dstPort = tcp.DestinationPort.ToString();
+
+                        var ipPacket = (IPPacket)tcp.ParentPacket;
+                        string srcAddress = ipPacket.SourceAddress.ToString();
+                        string dstAddress = ipPacket.DestinationAddress.ToString();
+                        
+                        tbPackets.AppendText($"Time:{time.Hour}:{time.Minute}:{time.Second} | Package Len = {lenPacket}" +
+                        $" | Type: TCP/IP | Source Port: {srcPort} | Destination Port: {dstPort} | Source IP: {srcAddress} | Destination IP: {dstAddress}\n");
+                    }
+                    if (deviceCaptured.Filter.Equals("udp"))
+                    {
+                        var udp = (UdpPacket)packetResult.GetPacket().Extract<UdpPacket>();
+                        if (udp != null)
+                        {
+                            DateTime time = packetResult.Timeval.Date;
+                            int lenPacket = packetResult.Data.Length;
+                            string srcPort = udp.SourcePort.ToString();
+                            string dstPort = udp.DestinationPort.ToString();
+
+                            var ipPacket = (IPPacket)udp.ParentPacket;
+                            string srcAddress = ipPacket.SourceAddress.ToString();
+                            string dstAddress = ipPacket.DestinationAddress.ToString();
+
+                            tbPackets.AppendText($"Time:{time.Hour}:{time.Minute}:{time.Second} | Package Len = {lenPacket}" +
+                            $" | Type: UDP | Source Port: {srcPort} | Destination Port: {dstPort} | Source IP: {srcAddress} | Destination IP: {dstAddress}\n");
+                        }
+                    }
+                }
+                
+            }
+        }
+
+        private void Device_OnPacketArrival(object sender, PacketCapture e)
+        {
+            if (isSniffing)
+            {
+                deviceCaptured = e.Device;
+                deviceCaptured.Filter = filters;
+                changePacketText(e.GetPacket());
+            }
+            else
+            {
+                e.Device.StopCapture();
+                e.Device.Close();
+            }
+        }
+
+        private void btnStopSniffing_Click(object sender, EventArgs e)
+        {
+            isSniffing = false;
+        }
+
+        private void btnTcp_Click(object sender, EventArgs e)
+        {
+            tbPackets.AppendText("TCP/IP Filter Applied:\n");
+            filters = "ip and tcp";
+            deviceCaptured.Filter = filters;
+
+        }
+
+        private void btnRemoveFilters_Click(object sender, EventArgs e)
+        {
+            tbPackets.AppendText("No Filters Applied:\n");
+            filters = string.Empty;
+            deviceCaptured.Filter = filters;
+        }
+
+        private void btnUDP_Click(object sender, EventArgs e)
+        {
+            tbPackets.AppendText("UDP Filter Applied:\n");
+            filters = "udp";
+            deviceCaptured.Filter = filters;
+        }
     }
 }
