5cc434071aa767955888033dc6f58e6390863394
TeresaBrilha <2191207@my.ipleiria.pt>
2022-11-02 14:43:35 +0000

    Display Packets Front

--- IDS_Project/Form1.Designer.cs
+++ IDS_Project/Form1.Designer.cs
@@ -33,6 +33,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.TbVersion = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.tbPackets = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // StartSnifferBtn
@@ -59,9 +61,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 114);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.Size = new System.Drawing.Size(46, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Dispositivos";
+            this.label1.Text = "Devices";
             // 
             // TbVersion
             // 
@@ -73,17 +75,36 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 91);
+            this.label2.Location = new System.Drawing.Point(9, 91);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(101, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "SharpPcap Version:";
+            // 
+            // tbPackets
+            // 
+            this.tbPackets.Location = new System.Drawing.Point(12, 259);
+            this.tbPackets.Name = "tbPackets";
+            this.tbPackets.Size = new System.Drawing.Size(776, 96);
+            this.tbPackets.TabIndex = 5;
+            this.tbPackets.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 243);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Packets";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbPackets);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.TbVersion);
             this.Controls.Add(this.label1);
@@ -103,6 +124,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox TbVersion;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox tbPackets;
+        private System.Windows.Forms.Label label3;
     }
 }
 
